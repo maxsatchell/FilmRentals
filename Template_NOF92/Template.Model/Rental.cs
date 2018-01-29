1f131da22723c89dce1f3d9f9d70770f75e84251
Maximilian Satchell <14ict772@stowe.co.uk>
2018-01-29 10:02:07 +0000

    Merger

---
+++ Template_NOF92/Template.Model/Rental.cs
@@ -0,0 +1,26 @@
+﻿using NakedObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Model
+{
+    public class Rental
+    {
+        
+
+        [NakedObjectsIgnore]//Indicates that this property will never be seen in the UI
+        public virtual int RentalID { get; set; }
+
+        public decimal price { get; set; }
+        public DateTime DateOut { get; set; }
+        public DateTime DateReturned { get; set; }
+        public DateTime DateReturnBy { get; set; }
+
+        public Customer Customer { get; set; }
+        public Film Film { get; set; }
+
+    }
+}
